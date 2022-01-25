d2d7b0c145e686eec88959c6914d831c0c49d171
Jose Soto <josecanhelp@gmail.com>
2022-01-24 22:29:05 -0800

    Refactor parameters and add all Accounts Endpoints

--- src/Stacks.php
+++ src/Stacks.php
@@ -16,11 +16,27 @@ class Stacks
         ]);
     }
 
-    public function accountBalances($principal, $queryParams = ['unanchored' => false, 'untilBlock' => null])
-    {
+    /**
+     * Get account balances
+     *
+     * @link https://docs.hiro.so/api#operation/get_account_balance
+     *
+     * @param string $principal
+     * @param bool $unanchored Include transaction data from unanchored (i.e. unconfirmed) microblocks
+     * @param string $untilBlock Returned data representing the state up until that point in time, rather than the current block.
+     * @return void
+     */
+    public function accountBalances(
+        $principal,
+        $unanchored = false,
+        $untilBlock = null
+    ) {
         $response = $this->client
                         ->get("extended/v1/address/{$principal}/balances", [
-                            'query' => $this->cleanQueryParams($queryParams),
+                            'query' => [
+                                'unanchored' => $unanchored,
+                                'until_block' => $untilBlock,
+                            ],
                         ])
                         ->getBody()
                         ->getContents();
@@ -28,11 +44,27 @@ class Stacks
         return json_decode($response, true);
     }
 
-    public function accountStxBalance($principal, $queryParams = ['unanchored' => null, 'untilBlock' => null])
-    {
+    /**
+     * Get account STX balance
+     *
+     * @link https://docs.hiro.so/api#operation/get_account_stx_balance
+     *
+     * @param string $principal
+     * @param bool $unanchored Include transaction data from unanchored (i.e. unconfirmed) microblocks
+     * @param string $untilBlock Returned data representing the state up until that point in time, rather than the current block.
+     * @return void
+     */
+    public function accountStxBalance(
+        $principal,
+        $unanchored = false,
+        $untilBlock = null
+    ) {
         $response = $this->client
                         ->get("extended/v1/address/{$principal}/stx", [
-                            'query' => $this->cleanQueryParams($queryParams),
+                            'query' => [
+                                'unanchored' => $unanchored,
+                                'until_block' => $untilBlock,
+                            ],
                         ])
                         ->getBody()
                         ->getContents();
@@ -40,11 +72,36 @@ class Stacks
         return json_decode($response, true);
     }
 
-    public function accountTransactions($principal, $queryParams = ['limit' => null, 'offset' => null, 'unanchored' => null, 'until_block' => null])
-    {
+    /**
+     * Get account transactions
+     *
+     * @link https://docs.hiro.so/api#operation/get_account_transactions
+     *
+     * @param string $principal
+     * @param bool $unanchored Include transaction data from unanchored (i.e. unconfirmed) microblocks
+     * @param string $untilBlock Returned data representing the state up until that point in time, rather than the current block.
+     * @param int $limit
+     * @param int $offset
+     * @param int $height
+     * @return void
+     */
+    public function accountTransactions(
+        $principal,
+        $unanchored = false,
+        $untilBlock = null,
+        $limit = null,
+        $offset = null,
+        $height = null,
+    ) {
         $response = $this->client
                         ->get("extended/v1/address/{$principal}/transactions", [
-                            'query' => $this->cleanQueryParams($queryParams),
+                            'query' => [
+                                'unanchored' => $unanchored,
+                                'until_block' => $untilBlock,
+                                'limit' => $limit,
+                                'offset' => $offset,
+                                'height' => $height,
+                            ],
                         ])
                         ->getBody()
                         ->getContents();
@@ -52,21 +109,55 @@ class Stacks
         return json_decode($response, true);
     }
 
-    public function accountTransactionForSpecificTransaction($principal, $transactionId)
-    {
+    /**
+     * Get account transaction information for a specific transaction
+     *
+     * @link https://docs.hiro.so/api#operation/get_single_transaction_with_transfers
+     *
+     * @param string $principal
+     * @param string $transactionId
+     * @return void
+     */
+    public function accountTransactionForSpecificTransaction(
+        $principal,
+        $transactionId
+    ) {
         $response = $this->client
-                        ->get("/extended/v1/address/{$principal}/{$transactionId}/with_transfers")
+                        ->get("extended/v1/address/{$principal}/{$transactionId}/with_transfers")
                         ->getBody()
                         ->getContents();
 
         return json_decode($response, true);
     }
 
-    public function recentTransactions($queryParams = ['limit' => null, 'offset' => null, 'type' => null, 'unanchored' => null])
-    {
+    /**
+     * Retrieve all transactions for an account or contract identifier including STX transfers for each transaction.
+     *
+     * @param string $principal
+     * @param bool $unanchored
+     * @param string $untilBlock
+     * @param int $limit
+     * @param int $offset
+     * @param int $height
+     * @return void
+     */
+    public function accountTransactionsWithStacksTransfers(
+        $principal,
+        $unanchored = false,
+        $untilBlock = null,
+        $limit = null,
+        $offset = null,
+        $height = null,
+    ) {
         $response = $this->client
-                        ->get('/extended/v1/tx', [
-                            'query' => $this->cleanQueryParams($queryParams),
+                        ->get("extended/v1/address/{$principal}/transactions_with_transfers", [
+                            'query' => [
+                                'unanchored' => $unanchored,
+                                'until_block' => $untilBlock,
+                                'limit' => $limit,
+                                'offset' => $offset,
+                                'height' => $height,
+                            ],
                         ])
                         ->getBody()
                         ->getContents();
@@ -74,10 +165,196 @@ class Stacks
         return json_decode($response, true);
     }
 
-    protected function cleanQueryParams($queryParams)
-    {
-        return array_filter($queryParams, function ($value) {
-            return $value !== null;
-        });
+    /**
+     * Get recent transactions
+     *
+     * @link https://docs.hiro.so/api#operation/get_transaction_list
+     *
+     * @param int $limit
+     * @param int $offset
+     * @param array<string> $type
+     * @param bool $unanchored Include transaction data from unanchored (i.e. unconfirmed) microblocks
+     * @return void
+     */
+    public function recentTransactions(
+        $limit = null,
+        $offset = null,
+        $type = null,
+        $unanchored = null
+    ) {
+        $response = $this->client
+                        ->get('extended/v1/tx', [
+                            'query' => [
+                                'limit' => $limit,
+                                'offset' => $offset,
+                                'type' => $type,
+                                'unanchored' => $unanchored,
+                            ],
+                        ])
+                        ->getBody()
+                        ->getContents();
+
+        return json_decode($response, true);
+    }
+
+    /**
+     * Get the latest nonce used by an account
+     *
+     * @link https://docs.hiro.so/api#operation/get_account_nonces
+     *
+     * @param string $principal
+     * @param int $block_height
+     * @param string $block_hash
+     * @return void
+     */
+    public function lastNonce(
+        $principal,
+        $block_height = null,
+        $block_hash = null,
+    ) {
+        $response = $this->client
+                        ->get("extended/v1/address/{$principal}/nonces", [
+                            'query' => [
+                                'block_height' => $block_height,
+                                'block_hash' => $block_hash,
+                            ],
+                        ])
+                        ->getBody()
+                        ->getContents();
+
+        return json_decode($response, true);
+    }
+
+    /**
+     * Get account assets
+     *
+     * @link https://docs.hiro.so/api#operation/get_account_assets
+     *
+     * @param string $principal
+     * @param bool $unanchored
+     * @param string $untilBlock
+     * @param int $limit
+     * @param int $offset
+     * @return void
+     */
+    public function accountAssets(
+        $principal,
+        $unanchored = false,
+        $untilBlock = null,
+        $limit = null,
+        $offset = null,
+    ) {
+        $response = $this->client
+                        ->get("extended/v1/address/{$principal}/assets", [
+                            'query' => [
+                                'unanchored' => $unanchored,
+                                'until_block' => $untilBlock,
+                                'limit' => $limit,
+                                'offset' => $offset,
+                            ],
+                        ])
+                        ->getBody()
+                        ->getContents();
+
+        return json_decode($response, true);
+    }
+
+    /**
+     * Get inbound STX transfers
+     *
+     * @link https://docs.hiro.so/api#operation/get_account_inbound
+     *
+     * @param string $principal
+     * @param bool $unanchored
+     * @param string $untilBlock
+     * @param int $limit
+     * @param int $offset
+     * @param string $height
+     * @return void
+     */
+    public function inboundStxTransfers(
+        $principal,
+        $unanchored = false,
+        $untilBlock = null,
+        $limit = null,
+        $offset = null,
+        $height = null,
+    ) {
+        $response = $this->client
+                        ->get("extended/v1/address/{$principal}/stx_inbound", [
+                            'query' => [
+                                'unanchored' => $unanchored,
+                                'until_block' => $untilBlock,
+                                'limit' => $limit,
+                                'offset' => $offset,
+                                'height' => $height,
+                            ],
+                        ])
+                        ->getBody()
+                        ->getContents();
+
+        return json_decode($response, true);
+    }
+
+    /**
+     * Get nft events
+     *
+     * @link https://docs.hiro.so/api#operation/get_account_nft
+     *
+     * @param string $principal
+     * @param bool $unanchored
+     * @param string $untilBlock
+     * @param int $limit
+     * @param int $offset
+     * @return void
+     */
+    public function nftEvents(
+        $principal,
+        $unanchored = false,
+        $untilBlock = null,
+        $limit = null,
+        $offset = null,
+    ) {
+        $response = $this->client
+                        ->get("extended/v1/address/{$principal}/nft_events", [
+                            'query' => [
+                                'unanchored' => $unanchored,
+                                'until_block' => $untilBlock,
+                                'limit' => $limit,
+                                'offset' => $offset,
+                            ],
+                        ])
+                        ->getBody()
+                        ->getContents();
+
+        return json_decode($response, true);
+    }
+
+    /**
+     * Get account info
+     *
+     * @link https://docs.hiro.so/api#operation/get_account_info
+     *
+     * @param string $principal
+     * @param int $proof
+     * @param string $tip
+     * @return void
+     */
+    public function accountInfo(
+        $principal,
+        $proof = null,
+        $tip = null,
+    ) {
+        $response = $this->client
+                        ->get("v2/accounts/{$principal}", [
+                            'query' => [
+                                'proof' => $proof,
+                                'tip' => $tip,
+                            ],
+                        ])
+                        ->getBody()
+                        ->getContents();
+
+        return json_decode($response, true);
     }
 }
--- tests/StacksTest.php
+++ tests/StacksTest.php
@@ -7,33 +7,71 @@ beforeEach(function () {
     $this->principal = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
 });
 
-it('can retrieve recent transactions', function () {
-    $response = $this->stacks->recentTransactions(['limit' => 1]);
-
-    $this->assertCount(1, $response['results']);
-});
-
-it('can retrieve account balances for a principal', function () {
+it('shows account balances for a principal', function () {
     $response = $this->stacks->accountBalances($this->principal);
 
     $this->assertTrue($response['stx']['balance'] > 0);
 });
 
-it('can retrieve account STX balance for a principal', function () {
+it('shows account STX balance for a principal', function () {
     $response = $this->stacks->accountStxBalance($this->principal);
 
     $this->assertTrue($response['balance'] > 0);
 });
 
-it('can retrieve account transactions', function () {
+it('shows account transactions', function () {
     $response = $this->stacks->accountTransactions($this->principal);
 
     $this->assertTrue($response['results'] > 0);
 });
 
-it('can retrieve account transaction information for a specific transaction', function () {
+it('shows account transaction information for a specific transaction', function () {
     $transactionId = $this->stacks->accountTransactions($this->principal)['results'][0]['tx_id'];
     $response = $this->stacks->accountTransactionForSpecificTransaction($this->principal, $transactionId);
 
     $this->assertTrue($response['tx']['tx_status'] != null);
+});
+
+it('shows account transactions with stacks transfers', function () {
+    $response = $this->stacks->accountTransactionsWithStacksTransfers($this->principal);
+
+    $this->assertTrue(count($response['results']) > 0);
+});
+
+it('shows the last nonce for an account', function () {
+    $response = $this->stacks->lastNonce($this->principal);
+
+    $this->assertTrue($response['possible_next_nonce'] > 0);
+});
+
+it('shows the account assets for a principal', function () {
+    $response = $this->stacks->accountAssets($this->principal);
+
+    $this->assertTrue(count($response['results']) > 0);
+});
+
+it('shows inbound STX transfers', function () {
+    $response = $this->stacks->inboundStxTransfers($this->principal);
+
+    $this->assertTrue(array_key_exists('results', $response));
+});
+
+it('shows all nfts owned by an address', function () {
+    $response = $this->stacks->nftEvents($this->principal);
+
+    $this->assertTrue(array_key_exists('total', $response));
+    $this->assertTrue(array_key_exists('nft_events', $response));
+});
+
+it('show account information for a principal', function () {
+    $response = $this->stacks->accountInfo($this->principal);
+
+    $this->assertTrue(array_key_exists('balance', $response));
+    $this->assertTrue(array_key_exists('nonce', $response));
+});
+
+it('shows recent transactions', function () {
+    $response = $this->stacks->recentTransactions(1);
+
+    $this->assertCount(1, $response['results']);
 });
